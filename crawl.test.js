05ecaffdf79a17a3f853bf671eeef7914086ffde
Ivan Bowen <itbowen@kabarak.ac.ke>
2023-04-02 17:48:38 +0300

    normalizeUrl test

--- crawl.test.js
+++ crawl.test.js
@@ -2,9 +2,30 @@ const {normalizeURL} = require('./crawl.js');
 const {test, expect} = require('@jest/globals');
 
 
-test('normalizeUrl', ()=> {
-   const input = '';
+test('normalizeUrl strip protocol', ()=> {
+   const input = 'https://boot.dev/path';
    const actual = normalizeURL(input);
-   const expected = '';
+   const expected = 'boot.dev/path';
+   expect(actual).toEqual(expected);
+})
+
+test('normalizeUrl strip trailing slash', ()=> {
+   const input = 'https://boot.dev/path/';
+   const actual = normalizeURL(input);
+   const expected = 'boot.dev/path';
+   expect(actual).toEqual(expected);
+})
+
+test('normalizeUrl capitals', ()=> {
+   const input = 'https://BOOT.dev/path/';
+   const actual = normalizeURL(input);
+   const expected = 'boot.dev/path';
+   expect(actual).toEqual(expected);
+})
+
+test('normalizeUrl strip http', ()=> {
+   const input = 'http://BOOT.dev/path/';
+   const actual = normalizeURL(input);
+   const expected = 'boot.dev/path';
    expect(actual).toEqual(expected);
 })
