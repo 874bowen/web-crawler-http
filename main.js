bb03c888426fd855592806fb7ab403f4ed078b0e
Ivan Bowen <itbowen@kabarak.ac.ke>
2023-04-04 00:09:29 +0300

    print pages

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { crawlPage } = require('./crawl.js')
 
-function main() {
+async function main() {
    if (process.argv.length < 3){
       console.log('No website provided');
       process.exit(1)
@@ -11,7 +11,12 @@ function main() {
    }
    const baseURL = process.argv[2]
    console.log(`Starting crawl of ${baseURL}... `);
-   crawlPage(baseURL)
+   const pages = await crawlPage(baseURL, baseURL, {});
+   console.log(pages);
+   for (const page in pages) {
+      console.log(page);
+   }
+
 }
 
 main()
